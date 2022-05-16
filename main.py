b337d3911ef05e1893faf626d7ed3b39404882b4
Skelebrinium <34858747+Skelebrinium@users.noreply.github.com>
2022-05-16 14:51:55 +0100

    changes to imports, prints and inputs
    functions may be broken as the changes have not been tested

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-import random
-import secrets
-import datetime
-import uuid
-import math
-import os
+from random import randint, choice
+from secret import token_hex
+from datetime import datetime
+from uuid import uuid4
+from math import sin, cos, tan, degrees, radians, e, pi, sqrt
+from os import listdir, mkdir, rmdir
+from sys import stdin, stdout
 
 class skel():
     # Advanced version of print()
     def createLine(text, type="log"):
         if type == "log" or type == "l" or type == 1:
-            return print(text)
+            return stdout.write(text + "\n")
         elif type == "warning" or type == "warn" or type == "w" or type == 2:
-            return print(f"Warning: {text}")
+            return stdout.write(f"Warning: {text}\n")
         elif type == "error" or type == "e" or type == 3:
-            return print(f"Error: {text}")
+            return stdout.write(f"Error: {text}\n")
         else:
-            return print("Invalid parameters.")
+            return stdout.write("Invalid parameters.\n")
 
     # Function for appending (to), overwriting or creating files
     def editFile(path = None, type = None, text = None):
@@ -37,15 +38,15 @@ class skel():
 
     # Make Directory
     def makeDir(path):
-        return os.mkdir(path)
+        return mkdir(path)
 
     # Remove Directory
     def removeDir(path):
-        return os.rmdir(path)
+        return rmdir(path)
 
     # List Directory
     def listDir(path):
-        return os.listdir(path)
+        return listdir(path)
 
     # Outputs the entirety of a file
     def readFile(path):
@@ -56,109 +57,111 @@ class skel():
 
     # Square Root Calculator
     def squareRoot(num1):
-        return math.sqrt(num1)
+        return sqrt(num1)
 
     # Sin Calculator
     def sin(num1):
-        return math.sin(num1)
+        return sin(num1)
 
     # Cos Calculator
     def cos(num1):
-        return math.cos(num1)
+        return cos(num1)
 
     # Tan Calculator
     def tan(num1):
-        return math.tan(num1)
+        return tan(num1)
 
     # Radian Calculator
     def radians(num1):
-        return math.radians(num1)
+        return radians(num1)
 
     # Degrees Calculator
     def degrees(num1):
-        return math.degrees(skel.pi/num1)
+        return degrees(skel.pi/num1)
 
     # Euler (e) value
     def Euler():
-        return math.e
+        return e
 
     # PI function
     def PI():
-        return math.pi
+        return pi
 
     # Time
     def getTime():
-        ctime = datetime.datetime.now()
-        return print(f"The time is {ctime.strftime('%y/%m/%d %H:%M')}")
+        ctime = datetime.now()
+        return stdout.write(f"The time is {ctime.strftime('%y/%m/%d %H:%M')}\n")
 
     # Random Number Generator
     def randNum(num1, num2):
-        return random.randint(num1, num2)
+        return randint(num1, num2)
 
     # Number Formatter
     def formatNum(num=1000, printResult=True):
         formattednumber = "{:,}".format(num)
         if printResult == True:
-            print(formattednumber)
+            stdout.write(formattednumber + "\n")
         elif printResult == False:
             return formattednumber
         else:
-            return print("The 'printResult' parameter is invalid.")
+            return stdout.write("The 'printResult' parameter is invalid.\n")
 
     # Gets a random item from an array
     def randItem(arrayname):
-        return random.choice(arrayname)
+        return choice(arrayname)
 
     # Randomized Key Generator
     def generateKey(type="uuid4", numOfChars=12):
         if type == "uuid4" or type == 4:
-            return print(f"UUID4: {str(uuid.uuid4())}")
+            return stdout.write(f"UUID4: {str(uuid4())}\n")
         elif type == "hextoken":
-            return print(secrets.token_hex(numOfChars))
+            return stdout.write(token_hex(numOfChars) + "\n")
         else:
-            return print("The 'type' parameter given is invalid.")
+            return stdout.write("The 'type' parameter given is invalid.\n")
 
     # Mathmatical operator (4 operations)
     def calculate(num1 = 9, operation = "add", num2 = 3, printresult = True):
         # Multiply operator (x or *)
         if operation == "x" or operation == "*" or operation == "X" or operation == "times" or operation == "times by" or operation == "multiplied by":
             if printresult == True:
-                return print(num1 * num2)
+                return stdout.write(num1 * num2 + "\n")
             elif printresult == False:
                 return num1 * num2
             else:
-                print(f"You didn't specify 'True' or 'False'. Your given parameter was '{printresult}'.")
+                stdout.write(f"You didn't specify 'True' or 'False'. Your given parameter was '{printresult}'.\n")
 
         # Subtract operator (-)
         elif operation == "takeaway" or operation == "take away" or operation == "minus" or operation == "-" or operation == "subtracted by":
             if printresult == True:
-                return print(num1 - num2)
+                return stdout.write(num1 - num2 + "\n")
             elif printresult == False:
                 return num1 - num2
             else:
-                print(f"You didn't specify 'True' or 'False'. Your given parameter was '{printresult}'.")
+                stdout.write(f"You didn't specify 'True' or 'False'. Your given parameter was '{printresult}'.\n")
 
         # Division operator (÷)
         elif operation == "divided by" or operation == "÷" or operation == "//":
             if printresult == True:
-                return print(num1 // num2)
+                return stdout.write(num1 // num2 + "\n")
             elif printresult == False:
                 return num1 // num2
             else:
-                print(f"You didn't specify 'True' or 'False'. Your given parameter was '{printresult}'.")
+                stdout.write(f"You didn't specify 'True' or 'False'. Your given parameter was '{printresult}'.\n")
 
         # Addition operator (+)
         elif operation == "plus" or operation == "+" or operation == "add":
             if printresult == True:
-                return print(num1 + num2)
+                return stdout.write(num1 + num2 + "\n")
             elif printresult == False:
                 return num1 + num2
             else:
-                print(f"You didn't specify 'True' or 'False'. Your given parameter was '{printresult}'.")
+                stdout.write(f"You didn't specify 'True' or 'False'. Your given parameter was '{printresult}'.\n")
 
         else:
-            print(f"That isn't an operation. Your given operation was '{operation}'.")
+            stdout.write(f"That isn't an operation. Your given operation was '{operation}'.\n")
 
     # Request user to input anything
     def requestUserInput(text = None):
-        return input(text)
+        stdout.write(text)
+        stdout.flush()
+        return stdin.readline()
